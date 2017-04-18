e55c6d2fe1e1ed6af86219a545c3eac717054511
Mohamed Abbas <softabbas@gmail.com>
2017-04-18 11:57:00 +0530

    align

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'admin',
-  database : 'mlztransportnew'
+  database : 'demo11'
 
 });
 var bodyParser = require('body-parser');
