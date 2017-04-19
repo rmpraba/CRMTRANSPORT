03f6e6853b3251da6e1d878c6eaedc2aea4f4e7e
rmpraba <rmpraba@gmail.com>
2017-04-19 06:54:23 +0530

    transport zone fees removed in zone and accadmin screen

--- server.js
+++ server.js
@@ -1317,7 +1317,7 @@ app.post('/getnameofstu-card',  urlencodedParser,function (req, res)
 app.post('/payfee-card',  urlencodedParser,function (req, res)
 {
    
-        var instalment1date=req.query.instdate;
+    var instalment1date=req.query.instdate;
     var mode;
     var install1;
     var install1date;
