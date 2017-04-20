46cdffb8c4dfe6cf7e801958cfb2db851c3ffd5e
janani1995 <cutesuja24@gmail.com>
2017-04-20 10:30:41 +0530

    tranport align

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'admin',
-  database : 'demo11'
+  database : 'transportdemo11'     
 
 });
 var bodyParser = require('body-parser');
