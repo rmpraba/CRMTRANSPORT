a39a737015556545d710d7bd12462def0c241fbf
Mohamed Abbas <softabbas@gmail.com>
2017-04-20 18:24:28 +0530

    align

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'admin',
-  database : 'transport'
+  database : 'transcloud'
 
 });
 var bodyParser = require('body-parser');
